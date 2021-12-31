34c7067c92b7842121808897a7e3a761f8b5d7d2
pomodorozhong <pomodorozhong@gmail.com>
2021-12-31 22:33:45 +0800

    feat✨(listing_all_file_here): ask for output path
    when there is none.

--- listing_all_file_here.py
+++ listing_all_file_here.py
@@ -3,12 +3,17 @@ import os
 import json
 import sys
 
+try:
+    arg_command = sys.argv[1]
+except IndexError:
+    print("you are missing the path of output file.")
+    exit()
 
 abs_path = os.path.abspath(os.getcwd())
 dest_dir = os.path.join(abs_path, "*")
 ordered = sorted(glob.glob(dest_dir))
 
-file_out = sys.argv[1]
+file_out = arg_command
 out = json.dumps(ordered)
 with open(file_out, "w") as fp:
     print(out, file=fp)
